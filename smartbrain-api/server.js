ea30d0d28535b5e1f544d18352ad9a1ad2165494
yoowebdevlife <yoowebdev@gmail.com>
2019-05-30 21:16:06 -0500

    Setting up server w/ /signin and /register

---
+++ smartbrain-api/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const database = {
+	users: [
+	 {
+	 	id: '123',
+	 	name: 'John',
+	 	email: 'john@gmail.com',
+	 	password: 'cookies',
+	 	entries: 0,
+	 	joined: new Date()
+	 },
+	 {
+	 	id: '124',
+	 	name: 'Sally',
+	 	email: 'sally@gmail.com',
+	 	password: 'bananas',
+	 	entries: 0,
+	 	joined: new Date()
+	 }
+	]
+}
+
+app.get('/', (req, res) => {
+	res.json(database.users);
+})
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+		res.json('A user is signedin!');
+	}
+	else {
+		res.status(400).json('Error logging in.');
+	}
+})
+
+app.post('/register', (req, res) => {
+	const {email, name, password} = req.body;
+	database.users.push({
+		id: '125',
+	 	name: name,
+	 	email: email,
+	 	password: password,
+	 	entries: 0,
+	 	joined: new Date()
+	})
+
+	res.json(database.users[database.users.length -1]);
+})
+
+app.listen(3000, () => {
+	console.log('Listening on PORT 3000');
+})
+
+/*
+/ --> GET --> res = this is working
+/signin --> POST = success/fail
+/register ---> POST = new user
+/profile/:userId --> GET = user
+/image --> PUT = user entry count
+*/
