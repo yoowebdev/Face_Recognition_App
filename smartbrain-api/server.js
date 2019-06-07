0f603adedae852f27f6f6ba0d241511bd9d097a5
yoowebdevlife <yoowebdev@gmail.com>
2019-06-06 20:23:41 -0500

    Setting up your Database, Connecting to the Database, Registering a user part 1

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -1,7 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "iamchrisyoo",
+    password: "",
+    database: "smart-brain"
+  }
+});
+
+db.select("*")
+  .from("users")
+  .then(data => {
+    console.log(data);
+  });
 
 const app = express();
 
@@ -9,87 +26,88 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const database = {
-	users: [
-	 {
-	 	id: '123',
-	 	name: 'John',
-	 	email: 'john@gmail.com',
-	 	password: 'cookies',
-	 	entries: 0,
-	 	joined: new Date()
-	 },
-	 {
-	 	id: '124',
-	 	name: 'Sally',
-	 	email: 'sally@gmail.com',
-	 	password: 'bananas',
-	 	entries: 0,
-	 	joined: new Date()
-	 }
-	],
-	login: [
-	 {
-	 	id: '987',
-	 	hash: '',
-	 	email: 'john@gmail.com'
-	 }
-	]
-}
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: "987",
+      hash: "",
+      email: "john@gmail.com"
+    }
+  ]
+};
 
-app.get('/', (req, res) => {
-	res.json(database.users);
-})
+app.get("/", (req, res) => {
+  res.json(database.users);
+});
 
-app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	}
-	else {
-		res.status(400).json('Error logging in.');
-	}
-})
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json("Error logging in.");
+  }
+});
 
-app.post('/register', (req, res) => {
-	const {email, name, password} = req.body;
-	database.users.push({
-		id: '125',
-	 	name: name,
-	 	email: email,
-	 	entries: 0,
-	 	joined: new Date()
-	})
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  db("users")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(console.log);
+  res.json(database.users[database.users.length - 1]);
+});
 
-	res.json(database.users[database.users.length -1]);
-})
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Not found");
+  }
+});
 
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		} 
-	})
-	if (!found) {
-		res.status(400).json('Not found');
-	}
-})
-
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-		res.status(400).json('Not found');
-	}
-})
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Not found");
+  }
+});
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
 //     // Store hash in your password DB.
@@ -104,5 +122,5 @@ app.put('/image', (req, res) => {
 // });
 
 app.listen(3000, () => {
-	console.log('Listening on PORT 3000');
-})
+  console.log("Listening on PORT 3000");
+});
